e9cbf6d8a53e95ac54fa84092ae0950a90df32c0
Vannia-LeVe <vlevevannia@gmail.com>
2026-04-26 14:31:11 -0700

    Se agregó una clase para cumplir con el primer paso de caso de prueba de iniciar sesión como administrador.

---
+++ src/main/java/mx/itson/gestioncitasmedicas/service/AutenticacionService.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.itson.gestioncitasmedicas.service;
+import mx.itson.gestioncitasmedicas.db.DatabaseConnection;
+import java.sql.*;
+/**
+ *
+ * @author Vanni
+ */
+
+
+public class AutenticacionService {
+
+    public static boolean login(String usuario, String contrasena) {
+        String sql = "SELECT * FROM usuario WHERE nombre_usuario = ? AND contrasena = ? AND activo = 1";
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, usuario);
+            pstmt.setString(2, contrasena); 
+         
+            ResultSet rs = pstmt.executeQuery();
+            return rs.next();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
